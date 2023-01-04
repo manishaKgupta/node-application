4928a0ad840abc299c91d943e308a38a58cd1e8d
Manisha Kumari Gupta <64262752+manishaKgupta@users.noreply.github.com>
2023-01-04 15:03:05 +0530

    Add files via upload

--- script/stop.sh
+++ script/stop.sh
@@ -2,4 +2,15 @@
 echo "Stopping nodejs app"
 sleep 10
 cd /home/ubuntu/node-application/
-sudo pm2 stop app.js
+#sudo pm2 stop app.js
+
+//Check if node process is running and then kill if exixts
+set -e
+node_id=`ps -aef | grep .pm2 | awk '{print $2}'`
+if [ !-z "$node_id"]
+   then
+      echo node service is not running
+   else
+      kill -9 $node_id
+      echo node  process killed forcefully, process id $node_id.
+   fi
